b64919199d62e8021ab7b7ede0ab227ed622b4b2
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-22 14:16:26 +0530

    added

--- Labs/Day-1/Day-1/Class1.cs
+++ Labs/Day-1/Day-1/Class1.cs
@@ -15,7 +15,7 @@ namespace Day_1
             b = c;
             Console.WriteLine("Value of a and b in swapping a=" + a +" b= "+b);
         }
-        public static void Main()
+        public static void Main3()
         {
 
             int a = 10, b = 20;
---
+++ Labs/Day-1/Day-1/Class3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_1
+{
+    class Class3
+    {
+        /// <summary>
+        /// Input from user
+        /// </summary>
+        public static void Main()
+        {
+            //string Name;
+            //Console.WriteLine("Please enter your name");
+            //Name = Console.ReadLine();
+            //Console.WriteLine("Input from user is : "+Name);
+
+            int Number1, Number2,Sum;
+
+            Console.WriteLine("Please enter the value of Number 1");
+            Number1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value of Number 2");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            Sum = Number1 + Number2;
+            Console.WriteLine("Sum of "+Number1+"+"+Number2+"= "+Sum);
+        }
+    }
+}
