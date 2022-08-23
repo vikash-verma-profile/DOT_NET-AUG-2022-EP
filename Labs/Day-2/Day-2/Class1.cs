22658021eaa06d8b95ccb713b941e868428d145b
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-23 10:01:57 +0530

    added loops

---
+++ Labs/Day-2/Day-2/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_2
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            bool flag = true;
+            int Number1=0, Number2=0;
+            while (flag)
+            {
+                Console.WriteLine("Enter your choice \n 1.Add \n 2.Sub \n 3.Divide \n 4.Multiplication \n 5.Exit");
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                if (Choice < 5)
+                {
+                    Console.WriteLine("Please enter value of Number 1");
+                    Number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter value of Number 2");
+                    Number2 = Convert.ToInt32(Console.ReadLine());
+                }
+                Calculator(Choice, Number1, Number2,ref flag);
+            }
+            Console.WriteLine("Please come again!!!");
+        }
+        static void Calculator(int Choice, int Number1, int Number2,ref bool flag)
+        {
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine("Sum of " + Number1 + "+" + Number2 + "=" + (Number1 + Number2));
+                    break;
+                case 2:
+                    Console.WriteLine("Sum of " + Number1 + "-" + Number2 + "=" + (Number1 - Number2));
+                    break;
+                case 3:
+                    Console.WriteLine("Sum of " + Number1 + "/" + Number2 + "=" + (Number1 / Number2));
+                    break;
+                case 4:
+                    Console.WriteLine("Sum of " + Number1 + "*" + Number2 + "=" + (Number1 * Number2));
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid input");
+                    break;
+            }
+        }
+    }
+}
---
+++ Labs/Day-2/Day-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("======================for loop========");
+            //for loop
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("======================while loop========");
+            //while loop  entry controlled loop
+            int counter = 0;
+            while (1!=1)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            Console.WriteLine("======================do while loop========");
+            //do while exit controlled loop
+            counter = 0;
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (1 != 1);
+
+
+            //foreach (var item in collection)
+            //{
+
+            //}
+        }
+    }
+}
