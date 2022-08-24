1e75f7a658cb594af05eb1dc7d1806bbcc06c1e0
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-24 09:53:33 +0530

    dtringd

--- Labs/Day-3/Day-3/Class1.cs
+++ Labs/Day-3/Day-3/Class1.cs
@@ -10,10 +10,26 @@ namespace Day_3
         {
 
             String s = "Vikash Verma";
-            for (int i = 0; i < s.Length; i++)
-            {
-                Console.WriteLine(s[i]);
-            }
+
+            s = String.Concat("Hello ", s);
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    Console.WriteLine(s[i]);
+            //}
+            int indexvalue = s.IndexOf('V');
+            Console.WriteLine(indexvalue);
+            s=s.Replace("Vikash","Raj");
+            s = s.ToLower();
+            Console.WriteLine(s);
+            s = s.ToUpper();
+            Console.WriteLine(s);
+
+            string palindrome = "CHAR";
+            char []revr = palindrome.ToCharArray();
+            Array.Reverse(revr);
+            string rev = new String(revr);
+
+            Console.WriteLine((rev == palindrome) ? "Palidrome" : "Not a Palndrome");
         }
     }
 }
