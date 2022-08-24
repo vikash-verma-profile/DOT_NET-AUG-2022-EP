cd526ca0323ba4852c35a5d704e22cc985169266
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-24 09:21:10 +0530

    added code

---
+++ Labs/Day-3/Day-3/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_3
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            String s = "Vikash Verma";
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+        }
+    }
+}
---
+++ Labs/Day-3/Day-3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day_3
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            dynamic k = null;
+            Nullable<int> i = k;
+            Console.WriteLine(i);
+        }
+    }
+}
