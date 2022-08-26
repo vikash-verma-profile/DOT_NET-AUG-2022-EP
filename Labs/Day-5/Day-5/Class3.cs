1d1b023670ed0197a9545851adb3b84df162c0fe
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-26 11:44:43 +0530

    added problem statement

--- Labs/Day-5/Day-5/Class3.cs
+++ Labs/Day-5/Day-5/Class3.cs
@@ -4,11 +4,47 @@ using System.Text;
 
 namespace Day_5
 {
+    interface IVehiculo
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+    class Car : IVehiculo
+    {
+        int Fuel;
+
+        public Car(int fuel)
+        {
+            Fuel = fuel;
+        }
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("Car is driving");
+            }
+            else
+            {
+                Console.WriteLine("No fuel");
+            }
+        }
+
+        public bool Refuel(int amount)
+        {
+            Fuel = Fuel + amount;
+            return true;
+        }
+    }
     class Class3
     {
         public static void Main()
         {
-
+            Car c = new Car(0);
+            int fuel = int.Parse(Console.ReadLine());
+            if (c.Refuel(fuel))
+            {
+                c.Drive();
+            }
         }
     }
 }
