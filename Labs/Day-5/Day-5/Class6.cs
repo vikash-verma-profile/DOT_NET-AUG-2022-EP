059fbcbd8c8dc7a81fe46bbe05fe48278d150fcb
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-26 14:36:02 +0530

    added problem statement

--- Labs/Day-5/Day-5/Class6.cs
+++ Labs/Day-5/Day-5/Class6.cs
@@ -7,30 +7,35 @@ namespace Day_5
 
     class A
     {
-        public A()
-        {
-            Console.WriteLine("Hi I am A");
+        //public A()
+        //{
+        //    Console.WriteLine("Hi I am A");
+        //}
+
+        public A(int a) {
+            Console.WriteLine("Value of a IN A is "+a);
         }
+
     }
     class B:A
     {
-        public B()
+        public B(int a):base(a)
         {
-            Console.WriteLine("Hi I am B");
+            Console.WriteLine("Value of a in B is " + a);
         }
     }
     class C:B
     {
-        public C()
+        public C(int a):base(a)
         {
-            Console.WriteLine("Hi I am C");
+            Console.WriteLine("Value of a in C is " + a);
         }
     }
     class Class6
     {
         public static void Main()
         {
-            C c = new C();
+            C c = new C(1);
         }
       
 
