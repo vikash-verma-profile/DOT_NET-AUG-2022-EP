a1c01de87bbdd62b3d7866d902fc64de377f1800
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-26 09:24:07 +0530

    added problem statement

---
+++ Labs/Day-5/Day-5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day_5
+{
+    class PhotoBook
+    {
+       protected int numPages;
+       
+        public PhotoBook()
+        {
+            numPages = 16;
+        }
+        public PhotoBook(int _numpages)
+        {
+            numPages = _numpages;
+        }
+        public int  GetNumberPages()
+        {
+            return numPages;
+        }
+    }
+
+    class BigPhotoBook: PhotoBook
+    {
+        public BigPhotoBook()
+        {
+            numPages = 64;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoBook photoBook1 = new PhotoBook();
+            Console.WriteLine(photoBook1.GetNumberPages());
+
+            PhotoBook photoBook2 = new PhotoBook(24);
+            Console.WriteLine(photoBook2.GetNumberPages());
+
+            BigPhotoBook photoBook3 = new BigPhotoBook();
+            Console.WriteLine(photoBook3.GetNumberPages());
+        }
+    }
+}
