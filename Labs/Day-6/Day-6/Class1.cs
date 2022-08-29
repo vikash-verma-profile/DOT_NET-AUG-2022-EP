5640f595b0fd992a475f30d51a938b9bef361012
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-29 11:16:52 +0530

    added code

---
+++ Labs/Day-6/Day-6/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_6
+{
+
+
+    /// <summary>
+    /// ENUMS
+    /// </summary>
+    /// 
+    enum months
+    {
+        jan=1,
+        feb=2,
+        march=3
+    }
+    enum Status
+    {
+        Inactive,
+        Active
+    }
+    class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine((int)months.feb);
+            Console.WriteLine((int)Status.Inactive);
+            Console.WriteLine(Status.Active);
+        }
+    }
+}
--- Labs/Day-6/Day-6/Program.cs
+++ Labs/Day-6/Day-6/Program.cs
@@ -14,15 +14,21 @@ namespace Day_6
         public string Name;
         public int Age;
         public int Weight;
+
+        public void getdata()
+        {
+
+        }
     }
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Person person;
             person.Name = "Vikash Verma";
             person.Weight = 90;
             person.Age = 43;
+            person.getdata();
 
             Console.WriteLine("Details of a person are :");
             Console.WriteLine("Name : "+person.Name);
