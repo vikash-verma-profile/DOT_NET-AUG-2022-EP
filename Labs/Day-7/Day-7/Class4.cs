a3fdfc8366bfe95c203748e991e28f0ce0fa1f7c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-30 12:22:40 +0530

    Multi cast delegate

--- Labs/Day-7/Day-7/Class4.cs
+++ Labs/Day-7/Day-7/Class4.cs
@@ -1,42 +1,49 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace Day_7
 {
-    class Class3
+    
+    /// <summary>
+/// generic collections work on the specific type is specified in the program 
+/// non-generic collections work on object type
+/// </summary>
+    class Class4
     {
-        public static void Main4()
+        public static void Main()
         {
-            Hashtable ht = new Hashtable();
-            ht.Add("1", "Vikash Vema 1");
-            ht.Add("2", "Vikash Vema 2");
-            ht.Add("3", "Vikash Vema 3");
-            ht.Add("4", "Vikash Vema 4");
-            ht.Add("5", "Vikash Vema 5");
-
-            //DictionaryEntry is a class whose object represents the data in a combination of key and value
-            //pairs
-            //var hash =ht.Keys.Cast<string>().OrderBy(x=>x);
-            foreach (DictionaryEntry item in ht)
+            List<int> lst = new List<int>();
+            lst.Add(100);
+            lst.Add(200);
+            foreach (var item in lst)
             {
-                Console.WriteLine(item.Key+"===>"+item.Value);
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("==================Dictionary==============");
+            Dictionary<int, string> dt = new Dictionary<int, string>();
+            dt.Add(1,"cs.net");
+            dt.Add(2, "vb.net");
+            dt.Add(3, "asp.net");
+            dt.Add(4, "vb.net");
+            foreach (KeyValuePair<int,string> item in dt)
+            {
+                Console.WriteLine(item.Key + "===>" + item.Value);
             }
 
             //
             /*
-             * Combination of arraylist and hashtable
+             *
              */
             Console.WriteLine("==================================Sorted List=================");
-            SortedList sl = new SortedList();
+            SortedList<string,string> sl = new SortedList<string, string>();
             Employee e = new Employee();
-            sl.Add("101", e);
-            sl.Add("cs","cs.net");
+           // sl.Add("101", e);
+            sl.Add("cs", "cs.net");
             sl.Add("asp", "asp.net");
 
-            foreach (DictionaryEntry item in sl )
+            foreach (KeyValuePair<string, string> item in sl)
             {
                 Console.WriteLine(item.Key + "===>" + item.Value);
             }
@@ -45,7 +52,7 @@ namespace Day_7
             * Stack
             */
             Console.WriteLine("==================================Stack=================");
-            Stack st = new Stack();
+            Stack<string> st = new Stack<string>();
             st.Push("Cs.net");
             st.Push("vb.net");
             st.Push("asp.net");
@@ -58,7 +65,7 @@ namespace Day_7
            * Queue
            */
             Console.WriteLine("==================================Queue=================");
-            Queue q= new Queue();
+            Queue<string> q = new Queue<string>();
             q.Enqueue("Cs.net");
             q.Enqueue("vb.net");
             q.Enqueue("asp.net");
@@ -66,6 +73,7 @@ namespace Day_7
             {
                 Console.WriteLine(item);
             }
+
         }
     }
 }
