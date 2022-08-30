c4e4e2c835919dae3da1a1dab4a4220d5367620e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-30 14:14:02 +0530

    Multi cast delegate

--- Labs/Day-7/Day-7/Class5.cs
+++ Labs/Day-7/Day-7/Class5.cs
@@ -12,18 +12,32 @@ namespace Day_7
     {
         public static void Main()
         {
-            string a = "AAAA";
-            string expectedPattern = "^[0-9]{5}$";
-            string stringPattern = "^[a-z]&[A-Z]{4}$";
-            Regex r = new Regex(stringPattern);
-            if (r.IsMatch(a.ToString()))
+            //string a = "AAAA";
+            //string expectedPattern = "^[0-9]{5}$";
+            //string stringPattern = "^[a-z]&[A-Z]{4}$";
+            //Regex r = new Regex(stringPattern);
+            //if (r.IsMatch(a.ToString()))
+            //{
+            //    Console.WriteLine("Valid Input");
+            //}
+            //else {
+
+            //    Console.WriteLine("Invalid Input");
+
+            //}
+
+            //validate valid mobile number
+
+            Regex r = new Regex(@"^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}");
+
+            string str = "+91-78271300";
+            if (r.IsMatch(str))
             {
-                Console.WriteLine("Valid Input");
+                Console.WriteLine("Valid mobile number");
             }
-            else {
-
-                Console.WriteLine("Invalid Input");
-
+            else
+            {
+                Console.WriteLine("Invalid Mobile Number");
             }
           
         }
