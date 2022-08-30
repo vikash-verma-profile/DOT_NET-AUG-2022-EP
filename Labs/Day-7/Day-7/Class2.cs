cf184f2e3eb2eabe66d147938cbd4926315ae02e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-08-30 11:48:41 +0530

    Multi cast delegate

--- Labs/Day-7/Day-7/Class2.cs
+++ Labs/Day-7/Day-7/Class2.cs
@@ -10,23 +10,55 @@ namespace Day_7
     /// 
     /// Non-generic   generic
     /// </summary>
+    /// 
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+    }
     class Class2
     {
-        public static void Main()
+        public static void Main3()
         {
             ArrayList al = new ArrayList();
-            string str = "Vikash Vermna";
-            int x = 7;
-            DateTime d = DateTime.Now;
 
-            al.Add(str);
+            Employee emp = new Employee();
+
+            emp.EmployeeId = 101;
+            emp.EmployeeName = "Vikash";
+            al.Add(emp);
+
+            foreach (Employee item in al)
+            {
+                Console.WriteLine(item.EmployeeId);
+                Console.WriteLine(item.EmployeeName);
+            }
+
+            /*
+            al.Add(s);
+           // string str = "Vikash Vermna";
+            int x = 7;
+            int x1 = 89;
+            int x2 = 1;
+            int x3 = 71;
+            // DateTime d = DateTime.Now;
+
+           // al.Add(str);
             al.Add(x);
-            al.Add(d);
+            al.Add(x1);
+            al.Add(x2);
+            al.Add(x3);
+            //  al.Add(d);
+            //al.Clear();
+            //al.Remove(str);
+            al.Sort();
 
             foreach (var item in al)
             {
                 Console.WriteLine(item);
             }
+
+            */
         }
     }
 }
---
+++ Labs/Day-7/Day-7/Class3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day_7
+{
+    class Class3
+    {
+        public static void Main()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("1", "Vikash Vema 1");
+            ht.Add("2", "Vikash Vema 2");
+            ht.Add("3", "Vikash Vema 3");
+            ht.Add("4", "Vikash Vema 4");
+            ht.Add("5", "Vikash Vema 5");
+
+            //DictionaryEntry is a class whose object represents the data in a combination of key and value
+            //pairs
+            //var hash =ht.Keys.Cast<string>().OrderBy(x=>x);
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key+"===>"+item.Value);
+            }
+
+            //
+            /*
+             * Combination of arraylist and hashtable
+             */
+            Console.WriteLine("==================================Sorted List=================");
+            SortedList sl = new SortedList();
+            Employee e = new Employee();
+            sl.Add("101", e);
+            sl.Add("cs","cs.net");
+            sl.Add("asp", "asp.net");
+
+            foreach (DictionaryEntry item in sl )
+            {
+                Console.WriteLine(item.Key + "===>" + item.Value);
+            }
+
+            /*
+            * Stack
+            */
+            Console.WriteLine("==================================Stack=================");
+            Stack st = new Stack();
+            st.Push("Cs.net");
+            st.Push("vb.net");
+            st.Push("asp.net");
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+
+            /*
+           * Queue
+           */
+            Console.WriteLine("==================================Queue=================");
+            Queue q= new Queue();
+            q.Enqueue("Cs.net");
+            q.Enqueue("vb.net");
+            q.Enqueue("asp.net");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
