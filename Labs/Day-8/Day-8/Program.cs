bac514b0f1a3a5951317a4d4cf26caaa26b39fb7
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-01 09:24:05 +0530

    added code for read and write from a file

---
+++ Labs/Day-8/Day-8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Day_8
+{
+    /// <summary>
+    /// reading and writing in a file
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName+@"\Files\sample.txt";
+            //StreamWriter writer = new StreamWriter(projectDirectory);
+            //writer.WriteLine("Hi My name is vikash verma");
+            //writer.Close();
+
+            using (StreamWriter writer1 = new StreamWriter(projectDirectory))
+            {
+                writer1.WriteLine("Hi My name is vikash verma from using keyword");
+            }
+
+            string readText = File.ReadAllText(projectDirectory);
+            Console.WriteLine(readText);
+        }
+    }
+}
