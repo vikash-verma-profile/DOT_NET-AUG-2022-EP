581b830df2ba73980e56c050ed3bb48fc6b478fb
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-01 10:30:35 +0530

    added code for read and write from a file

---
+++ Labs/Day-8/Day-8/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace Day_8
+{
+    class student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Data
+    {
+        public student[] students { get; set; }
+    }
+    class Class1
+    {
+        public static void Main()
+        {
+            student[] students = new student[2];
+            for (int i = 0; i < students.Length; i++)
+            {
+                student s = new student();
+                Console.WriteLine("Please enter student Roll No");
+                s.RollNo=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter student Name");
+                s.Name=Console.ReadLine();
+                students[i] = s;
+            }
+            Data data = new Data();
+            data.students = students;
+            var jsonString =JsonConvert.SerializeObject(data);
+
+            //Console.WriteLine(jsonString);
+            string filePath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\Files\data.txt";
+            File.WriteAllText(filePath, jsonString);
+            string readText = File.ReadAllText(filePath);
+          //  Console.WriteLine(readText);
+            var obj = JsonConvert.DeserializeObject<Data>(readText);
+            foreach (var item in obj.students)
+            {
+                Console.WriteLine("Student ID"+item.RollNo);
+            }
+        }
+    }
+}
--- Labs/Day-8/Day-8/Program.cs
+++ Labs/Day-8/Day-8/Program.cs
@@ -6,22 +6,41 @@ namespace Day_8
     /// <summary>
     /// reading and writing in a file
     /// </summary>
+    /// 
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName+@"\Files\sample.txt";
             //StreamWriter writer = new StreamWriter(projectDirectory);
             //writer.WriteLine("Hi My name is vikash verma");
             //writer.Close();
 
-            using (StreamWriter writer1 = new StreamWriter(projectDirectory))
-            {
-                writer1.WriteLine("Hi My name is vikash verma from using keyword");
-            }
+            //using (StreamWriter writer1 = new StreamWriter(projectDirectory))
+            //{
+            //    writer1.WriteLine("Hi My name is vikash verma from using keyword");
+            //}
 
+
+
+
+            //string[] names = { "Vikash Verma","Suresh Kumar","Raj Kumar" };
+
+            Employee emp = new Employee();
+            emp.Id = 1;
+            emp.Name = "Sameer";
+
+            
+            File.WriteAllText(projectDirectory, "Id:"+emp.Id.ToString()+"Name:"+emp.Name);
             string readText = File.ReadAllText(projectDirectory);
             Console.WriteLine(readText);
+
+
         }
     }
 }
