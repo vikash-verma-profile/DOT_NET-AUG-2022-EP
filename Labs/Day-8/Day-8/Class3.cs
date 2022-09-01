c8f9bcc7629e5f16ca0169a403559bb5d2c9b918
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-01 14:55:10 +0530

    Threads

--- Labs/Day-8/Day-8/Class3.cs
+++ Labs/Day-8/Day-8/Class3.cs
@@ -13,13 +13,25 @@ namespace Day_8
     {
         public static void CallThread()
         {
-            for (int i = 0; i < 10; i++)
+            try
             {
-                Thread.Sleep(5000);//Miliseconds
-                Console.WriteLine(i);
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(5000);//Miliseconds
+                    Console.WriteLine(i);
+                }
             }
+            catch(ThreadAbortException ex)
+            {
+                Console.WriteLine("Thread abort");
+            }
+            finally
+            {
+                Console.WriteLine("Can't catch the thread exception");
+            }
+           
         }
-        public static void Main()
+        public static void Main4()
         {
             //Thread th = Thread.CurrentThread;
             //th.Name = "Main Thread";
@@ -28,7 +40,9 @@ namespace Day_8
             ThreadStart sampleThread = new ThreadStart(CallThread);
             Thread childThread = new Thread(sampleThread);
             childThread.Start();
-           
+            Thread.Sleep(2000);
+            //childThread.Abort();
+            childThread.Priority = ThreadPriority.Highest;
         }
     }
 }
---
+++ Labs/Day-8/Day-8/Class4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Day_8
+{
+    /// <summary>
+    /// =>
+    /// </summary>
+    class Class4
+    {
+        public static void Main()
+        {
+            List<int> number = new List<int>() { 1, 2, 3, 4 };
+            //1*1  2*2 3*3
+            foreach (var item in number)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine();
+            var square = number.Select(x => x * x);
+            var evenNumbers = number.FindAll(x => (x % 2)==0);
+            foreach (var item in square)
+            {
+                Console.Write(item +"\t");
+            }
+            Console.WriteLine();
+            foreach (var item in evenNumbers)
+            {
+                Console.Write(item + "\t");
+            }
+        }
+    }
+}
