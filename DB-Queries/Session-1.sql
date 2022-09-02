0b7c7ef483077e7d91e02d9b5257682bb2a6571b
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-02 10:49:47 +0530

    added sql codes

---
+++ DB-Queries/Session-1.sql
@@ -0,0 +1,7 @@
+Create Database DemoDB
+Use DemoDB
+Create Table Sample(Id int,Name Varchar(200))
+select * from Sample;
+Insert into Sample(Id,Name) Values(2,'Vikash')
+Update Sample SET Name='RAM'
+Update Sample SET Name='Vikash' where id=2
