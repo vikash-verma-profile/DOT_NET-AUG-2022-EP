db22a0f5b1f5cc68c02b274be227581ccfd52975
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-06 09:24:03 +0530

    added db connection

---
+++ Labs/Customers/Customers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Customers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter student Name");
+            string studentName = Console.ReadLine();
+
+            EmployeeDbContext db = new EmployeeDbContext();
+            //Create an object of the class that is replica of db table
+            TblStudent tblStudent = new TblStudent();
+            tblStudent.StudentName = studentName;
+            db.TblStudents.Add(tblStudent);
+            //save collection to database whatever we have updated
+            db.SaveChanges();
+            Console.WriteLine("db record added successfully");
+            
+        }
+    }
+}
