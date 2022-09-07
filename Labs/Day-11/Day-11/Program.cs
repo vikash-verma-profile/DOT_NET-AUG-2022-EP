6d12a4bd3ba6ff56296b64f96ed2b5be90ecfdef
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-07 13:13:04 +0530

    join

--- Labs/Day-11/Day-11/Program.cs
+++ Labs/Day-11/Day-11/Program.cs
@@ -9,11 +9,12 @@ namespace Day_11
         static void Main(string[] args)
         {
             DemoDBContext db = new DemoDBContext();
-            var data = db.Students.Join(db.Courses, x => x.CourseId, y => y.Id, (x, y) => new { Id=x.Id,firstName=x.Firstname,lastName=x.Lastname,CourseName=y.CourseName});
-            Console.WriteLine("ID | FirstName | LastName | CourseName");
+            //var data = db.Students.Join(db.Courses, x => x.CourseId, y => y.Id, (x, y) => new { Id=x.Id,firstName=x.Firstname,lastName=x.Lastname,CourseName=y.CourseName});
+            var data = db.Students.Join(db.Courses, x => x.CourseId, y => y.Id, (x, y) => new { x,y }).Join(db.CourseSubjects,z=>z.y.Id,c=>c.CourseId,(z,c)=>new { Id = z.x.Id, firstName = z.x.Firstname, lastName = z.x.Lastname, CourseName = z.y.CourseName,Subject=c.SubjectName });
+            Console.WriteLine("ID | FirstName | LastName | CourseName | SubjectName");
             foreach (var item in data)
             {
-                Console.WriteLine(item.Id + " | " + item.firstName + " | " + item.lastName + " | " + item.CourseName);
+                Console.WriteLine(item.Id + " | " + item.firstName + " | " + item.lastName + " | " + item.CourseName+ " | " + item.Subject);
             }
         }
     }
