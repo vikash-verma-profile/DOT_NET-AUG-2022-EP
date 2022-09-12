c7c2a710eb880a2a1cb5859dbab253fe47ffd04c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-12 11:56:13 +0530

    added

--- Labs/WebApp1/WebApp1/Startup.cs
+++ Labs/WebApp1/WebApp1/Startup.cs
@@ -51,7 +51,7 @@ namespace WebApp1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=list}/{id?}");
             });
         }
     }
