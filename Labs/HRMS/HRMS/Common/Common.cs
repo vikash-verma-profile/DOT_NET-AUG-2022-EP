9f2621c8ae45d958e10119a8731684aa6c64b952
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-14 13:18:41 +0530

    added register page

---
+++ Labs/HRMS/HRMS/Common/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMS.Common
+{
+    public enum EGender
+    {
+        Male=1,
+        Female=2,
+        Other=3
+    }
+    public class Common
+    {
+    }
+}
--- Labs/HRMS/HRMS/Controllers/HomeController.cs
+++ Labs/HRMS/HRMS/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using HRMS.ViewModel;
+﻿using HRMS.Common;
+using HRMS.Models;
+using HRMS.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,6 +13,7 @@ namespace HRMS.Controllers
 {
     public class HomeController : Controller
     {
+        HRMSDBContext db = new HRMSDBContext();
         public IActionResult Index()
         {
             return View();
@@ -33,7 +36,39 @@ namespace HRMS.Controllers
         [HttpPost]
         public IActionResult Register(RegisterViewModel register)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                TblUser user = new TblUser();
+                user.FirstName = register.FirstName;
+                user.LastName = register.LastName;
+                user.EmailId = register.EmailId;
+                user.Password = register.Password;
+                user.Dob = register.Dob;
+
+                switch (register.Gender)
+                {
+                    case "Male":
+                        {
+                            user.Gender = (int)EGender.Male;
+                            break;
+                        }
+                    case "Female":
+                        {
+                            user.Gender = (int)EGender.Female;
+                            break;
+                        }
+                    case "Other":
+                        {
+                            user.Gender = (int)EGender.Other;
+                            break;
+                        }
+
+                }
+                db.TblUsers.Add(user);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
