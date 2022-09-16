c0032d6416497b69911f38f920c6cdede44923a4
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-16 11:26:16 +0530

    added api code

--- Labs/HRMSAPI/HRMSAPI/Controllers/HomeController.cs
+++ Labs/HRMSAPI/HRMSAPI/Controllers/HomeController.cs
@@ -34,5 +34,13 @@ namespace HRMSAPI.Controllers
             db.SaveChanges();
             return Ok(new { Status = "record updated succesfully" });
         }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+           // db.TblUsers.Update(user);
+            db.SaveChanges();
+            return Ok(new { Status = "record updated succesfully" });
+        }
     }
 }
